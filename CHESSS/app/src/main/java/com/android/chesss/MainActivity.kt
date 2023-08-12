5ec66885c8322ef887db6b19a77ca5a83c47ac8a
Sneheelvirale <142049641+sneheelvirale@users.noreply.github.com>
2023-08-13 01:52:03 +0530

    Initial commit

--- CHESSS/app/src/main/java/com/android/chesss/MainActivity.kt
+++ CHESSS/app/src/main/java/com/android/chesss/MainActivity.kt
@@ -2,10 +2,16 @@ package com.android.chesss
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+
+private const val TAG = "MainActitvity"
 
 class MainActivity : AppCompatActivity() {
+    var chessModel = ChessModel()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Log.d(TAG, "$chessModel")
+
     }
 }
