7b189941123989d3ac3068d5a5b92af1957981ac
Sneheel Virale <viralesneheel@gmail.com>
2023-08-13 07:36:17 +0530

    Initial commit

--- CHESSS/app/src/main/java/com/android/chesss/ChessView.kt
+++ CHESSS/app/src/main/java/com/android/chesss/ChessView.kt
@@ -15,9 +15,38 @@ class ChessView(context: Context?, attrs: AttributeSet?) : View(context, attrs) 
     private final val originX = 20f
     private final val originY = 200f
     private final val cellSide = 130f
+    private final val imgResIDs = setOf(R.drawable.
+    black_bishop,
+        R.drawable.white_bishop,
+        R.drawable.black_king,
+        R.drawable.white_king,
+        R.drawable.black_queen,
+        R.drawable.white_queen,
+        R.drawable.black_rook,
+        R.drawable.white_rook,
+        R.drawable.black_knight,
+        R.drawable.white_knight,
+        R.drawable.black_pawn,
+        R.drawable.white_pawn
+    )
+    private final val bitmaps = mutableMapOf<Int, Bitmap>()
+    private final val paint = Paint()
+    init {
+        loadBitmaps()
+    }
 
     override fun onDraw(canvas: Canvas?) {
-        val paint = Paint()
+        drawChessboard(canvas)
+
+        val whiteQueenBitmap = bitmaps[R.drawable.white_queen]!!
+        canvas?.drawBitmap(whiteQueenBitmap, null, Rect(0,0,200,200), paint)
+    }
+    private fun loadBitmaps(){
+        imgResIDs.forEach {
+            bitmaps[it] = BitmapFactory.decodeResource(resources, it)
+        }
+    }
+    private fun drawChessboard(canvas: Canvas?){
         for (i in 0..7) {
 
             for (j in 0..7) {
@@ -32,8 +61,5 @@ class ChessView(context: Context?, attrs: AttributeSet?) : View(context, attrs) 
                 )
             }
         }
-        val whiteQueenBitmap: Bitmap
-        whiteQueenBitmap = BitmapFactory.decodeResource(resources, R.drawable.white_queen)
-        canvas?.drawBitmap(whiteQueenBitmap, null, Rect(0,0,100,100), paint)
     }
 }
