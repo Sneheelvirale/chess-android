13161d1b17474836df6b3f73970e99529c99db38
Sneheel Virale <viralesneheel@gmail.com>
2023-08-14 01:15:57 +0530

    Initial commit

--- CHESSS/app/src/main/java/com/android/chesss/ChessView.kt
+++ CHESSS/app/src/main/java/com/android/chesss/ChessView.kt
@@ -15,8 +15,8 @@ class ChessView(context: Context?, attrs: AttributeSet?) : View(context, attrs) 
     private final val originX = 20f
     private final val originY = 200f
     private final val cellSide = 130f
-    private final val imgResIDs = setOf(R.drawable.
-    bb,
+    private final val imgResIDs = setOf(
+        R.drawable.bb,
         R.drawable.wb,
         R.drawable.bk,
         R.drawable.wk,
@@ -31,6 +31,7 @@ class ChessView(context: Context?, attrs: AttributeSet?) : View(context, attrs) 
     )
     private final val bitmaps = mutableMapOf<Int, Bitmap>()
     private final val paint = Paint()
+
     init {
         loadBitmaps()
     }
@@ -40,47 +41,68 @@ class ChessView(context: Context?, attrs: AttributeSet?) : View(context, attrs) 
         drawPieces(canvas)
 
     }
-    private fun drawPieces(canvas: Canvas?){
+
+    private fun drawPieces(canvas: Canvas?) {
         val chessModel = ChessModel()
         chessModel.reset()
 
-        for (row in 0..7){
-            for (col in 0..7){
-                chessModel.pieceAt(col, row)?.let { drawPieceAt(canvas, col, row, it.resID)
+        for (row in 0..7) {
+            for (col in 0..7) {
+                chessModel.pieceAt(col, row)?.let {
+                    drawPieceAt(canvas, col, row, it.resID)
                 }
             }
         }
-    //    drawPieceAt(canvas, 0,7,R.drawable.wr)
-      //  drawPieceAt(canvas, 0,6,R.drawable.wp)
+        //    drawPieceAt(canvas, 0,7,R.drawable.wr)
+        //  drawPieceAt(canvas, 0,6,R.drawable.wp)
 
 
     }
-    private fun drawPieceAt(canvas: Canvas?, col: Int,row: Int, resID: Int) {
+
+    private fun drawPieceAt(canvas: Canvas?, col: Int, row: Int, resID: Int) {
         val bitmap = bitmaps[resID]!!
-        canvas?.drawBitmap(bitmap,
-            null, RectF(originX + col * cellSide,originY + ( 7 -row ) * cellSide,originX + (col + 1)  * cellSide,originY + (( 7 - row ) +1)  * cellSide), paint)
+        canvas?.drawBitmap(
+            bitmap,
+            null,
+            RectF(
+                originX + col * cellSide,
+                originY + (7 - row) * cellSide,
+                originX + (col + 1) * cellSide,
+                originY + ((7 - row) + 1) * cellSide
+            ),
+            paint
+        )
 
 
     }
-    private fun loadBitmaps(){
-        imgResIDs.forEach  {
+
+    private fun loadBitmaps() {
+        imgResIDs.forEach {
             bitmaps[it] = BitmapFactory.decodeResource(resources, it)
         }
     }
-    private fun drawChessboard(canvas: Canvas?){
-        for (i in 0..7) {
 
-            for (j in 0..7) {
-                paint.color = if ((i + j) % 2 == 1)Color.LTGRAY
-                else Color.DKGRAY
+    private fun drawChessboard(canvas: Canvas?) {
+        for (col in 0..7) {
+
+            for (row in 0..7) {
+                drawSquareAt(canvas, col, row, (col + row) % 2 == 1)
+
+                }
+                 }
+            }
+            private fun drawSquareAt(canvas: Canvas?, col: Int, row: Int, isDark: Boolean) {
+                paint.color = if (isDark) Color.BLUE
+                else Color.CYAN
                 canvas?.drawRect(
-                    originX + i * cellSide,
-                    originY + j * cellSide,
-                    originX + ( i + 1) * cellSide,
-                    originY + (j + 1) * cellSide,
+                    originX + col * cellSide,
+                    originY + row * cellSide,
+                    originX + (col + 1) * cellSide,
+                    originY + (row + 1) * cellSide,
                     paint
                 )
+
             }
-        }
-    }
 }
+
+
