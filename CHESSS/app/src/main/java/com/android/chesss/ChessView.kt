fa3e128753491556abbe942e646fc78c1c22abde
Sneheel Virale <viralesneheel@gmail.com>
2023-08-13 05:54:12 +0530

    Initial commit

---
+++ CHESSS/app/src/main/java/com/android/chesss/ChessView.kt
@@ -0,0 +1,33 @@
+package com.android.chesss
+
+import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
+import android.util.AttributeSet
+import android.view.View
+
+class ChessView(context: Context?, attrs: AttributeSet?) : View(context, attrs) {
+
+    private final val originX = 20f
+    private final val originY = 200f
+    private final val cellSide = 130f
+
+    override fun onDraw(canvas: Canvas?) {
+        val paint = Paint()
+        for (i in 0..7) {
+
+            for (j in 0..7) {
+                paint.color = if ((i + j) % 2 == 0)Color.LTGRAY
+                else Color.BLACK
+                canvas?.drawRect(
+                    originX + i * cellSide,
+                    originY + j * cellSide,
+                    originX + ( i + 1) * cellSide,
+                    originY + (j + 1) * cellSide,
+                    paint
+                )
+            }
+        }
+    }
+}
