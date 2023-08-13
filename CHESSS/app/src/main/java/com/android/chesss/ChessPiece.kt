6423ec8259ff8779a1b58ab60ad47c9affef7520
Sneheel Virale <viralesneheel@gmail.com>
2023-08-13 21:36:03 +0530

    Initial commit

--- CHESSS/app/src/main/java/com/android/chesss/ChessPiece.kt
+++ CHESSS/app/src/main/java/com/android/chesss/ChessPiece.kt
@@ -1,4 +1,4 @@
 package com.android.chesss
 
-data class ChessPiece(val col: Int, val row: Int, val player: ChessPlayer,val rank: ChessRank) {
+data class ChessPiece(val col: Int, val row: Int, val player: ChessPlayer,val rank: ChessRank, val resID: Int) {
 }
